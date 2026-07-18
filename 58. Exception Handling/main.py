014e0dd001ad95cb69a15c1e00c95437f71fd0b3
Aman Kumar <amank022008@gmail.com>
2026-07-19 00:03:39 +0530

    till part 58, exception handling done

--- 58. Exception Handling/main.py
+++ 58. Exception Handling/main.py
@@ -1,3 +1,15 @@
 # exception
 # An event that interrupts the flow of a program (ZeroDivisionError, TypeError, ValueError)
 # 1.try, 2.except, 3.finally
+
+try:
+    number = int(input("Enter your number:\t"))
+    print(1/number)
+except ZeroDivisionError:
+    print("1/0 not allowed")
+except ValueError:
+    print("Only Numbers pls")
+except Exception:
+    print("Something went wrong")
+finally:
+    print("Clean up")
